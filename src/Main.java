956ccd289c68357762b2917dd15674a1edc86a08
Wasilii92 <eup104st@yandex.ru>
2025-02-13 00:42:04 +0500

    Убрал двойную вложенность

--- src/Main.java
+++ src/Main.java
@@ -3,37 +3,30 @@ public class Main {
         char clientOS = '1';
         if (clientOS == '1') {
             System.out.println("Установите версию приложения Android по ссылке");
-        } else {
+        }
+        if (clientOS == '0') {
             System.out.println("Установите версию приложения iOS по ссылке");
         }
 
-        int clientDeviceYear = 2015;
-        if (clientDeviceYear < 2015) {
-            if (clientOS == '1') {
-                System.out.println("Установите облегченную версию Android по ссылке");
-            } else
-                System.out.println("Установите облегченную версию iOS по ссылке");
-        } else {
-            System.out.println("Установите приложение по ссылке");
+        int clientDeviceYear = 2012;
+        if (clientOS == '1' && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию Android по ссылке");
+        }
+        if (clientOS == '0' && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию iOS по ссылке");
+        }
+        if (clientOS == '1' && clientDeviceYear >= 2015) {
+            System.out.println("Установите приложение Android по ссылке");
+        }
+        if (clientOS == '0' && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения iOS по ссылке");
         }
 
         int year = 2025;
-        if (year < 1584) {
-            System.out.println(year + " не является  високосным.");
-        } else {
-            if (year % 400 == 0) {
-                System.out.println(year + " является високосным.");
-            } else {
-                if (year % 100 == 0) {
-                    System.out.println(year + " не является високосным.");
-                } else {
-                    if (year % 4 == 0) {
-                        System.out.println(year + " является високосным.");
-                    } else {
-                        System.out.println(year + " не является високосным.");
-                    }
-                }
-            }
+        if ((((year % 100 != 0) && (year % 4 == 0)) || (year % 400 == 0)) && (year > 1584))
+            System.out.println(year + " является  високосным.");
+        else {
+            System.out.println(year + " не является високосным.");
         }
 
         int deliveryDistance = 95;
@@ -50,40 +43,24 @@ public class Main {
         int monthNumber = 12;
         switch (monthNumber) {
             case 1:
-                System.out.println("Зима");
-                break;
             case 2:
+            case 12:
                 System.out.println("Зима");
                 break;
             case 3:
-                System.out.println("Весна");
-                break;
             case 4:
-                System.out.println("Весна");
-                break;
             case 5:
                 System.out.println("Весна");
                 break;
             case 6:
-                System.out.println("Лето");
-                break;
             case 7:
-                System.out.println("Лето");
-                break;
             case 8:
                 System.out.println("Лето");
                 break;
             case 9:
-                System.out.println("Осень");
-                break;
             case 10:
-                System.out.println("Осень");
-                break;
             case 11:
                 System.out.println("Осень");
-                break;
-            case 12:
-                System.out.println("Зима");
                 break;
             default:
                 System.out.println("Такого месяца не существует");
